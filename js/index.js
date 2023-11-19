d533a5032fe3c78046976abd5fd1b834891122be
MatiasValentin <colombo.matiasvalentin@gmail.com>
2023-11-19 21:50:51 +0100

    agregando alerta

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
-alert("hola!")
-
 let visita = prompt ("Coloca tu nombre");
+ 
+alert (`Hola ${visita }! mi nombre es Matias Colombo. Soy musico y productor, 
+estoy trabajando musica que traigo desde hace mucho tiempo, y para mantenerme activo como musico, 
+cumplo tambien con mi trabajo como musico sesionista, donde cumplo con mi funcion como bajista electrico.
+ Estudie en Conservatorio de Musica Popular de Avellaneda, y toque en una gran cantidad de grupos, 
+ donde a lo largo del recorrido fui creando contenidos propios. 
+ Soy aficionado de la musica rock desde los 50/60 hasta los 2010, 
+ mayormente escucho rock electronico de la ultima decada, aprendiendo asi sobre sonido y produccion`);
 
-alert ("Hola " + visita);
